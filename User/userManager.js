b8974e0f104497183bfdcc8f393ca8e7be1a8227
RODRIGO NEGRON <rodrigoo.n@hotmail.com.argit config --global user.email rodrigoo.n@hotmail.com.ar>
2021-07-02 17:29:47 -0300

    user whoami method created

--- User/userManager.js
+++ User/userManager.js
@@ -2,13 +2,12 @@ const Super = require("./super");
 const Regular = require("./regular");
 
 function userManager(userList, command, argument1, argument2, argument3) {
-
   if (command.toLowerCase() === "exit") {
     console.log("\nExiting!\n");
     process.exit(0);
 
   } else if (command.toLowerCase() === "create_user") {
-    console.log("todo el obj", userList)
+    console.log("todo el obj", userList);
 
     userList.loggedUser && userList.loggedUser instanceof Super
       ? userList.createUser(argument1, argument2, argument3)
@@ -17,22 +16,25 @@ function userManager(userList, command, argument1, argument2, argument3) {
   } else if (command.toLowerCase() === "login") {
     userList.checkCredentials(argument1, argument2);
 
-  }else if (command.toLowerCase() === "logout") {
+  } else if (command.toLowerCase() === "logout") {
     userList.logOut();
 
-  }else if (command.toLowerCase() === "update_password") {
+  } else if (command.toLowerCase() === "update_password") {
     userList.loggedUser instanceof Super ||
-    userList.loggedUser instanceof Regular ?
-    userList.updatePassword(argument1, userList.loggedUser.userName)
-    : console.log(`you don't have permission to access this method`);
+    userList.loggedUser instanceof Regular
+      ? userList.updatePassword(argument1, userList.loggedUser.userName)
+      : console.log(`you don't have permission to access this method`);
 
-  }else if (command.toLowerCase() === "destroy_user") {
-    userList.loggedUser instanceof Super ?
-    userList.deleteUser(argument1)
-    : console.log(`you don't have permission to access this method`);
-
+  } else if (command.toLowerCase() === "destroy_user") {
+    userList.loggedUser instanceof Super
+      ? userList.deleteUser(argument1)
+      : console.log(`you don't have permission to access this method`);
+      
+  } else if (command.toLowerCase() === "whoami") {
+    if (userList.loggedUser) {
+      console.log("Logged user: ", userList.loggedUser.userName);
+    }
   }
 }
 
 module.exports = userManager;
-
